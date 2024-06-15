7c5793a002c3232833530aef667501f287648cfd
Junosprite007 <Junosprite007@gmail.com>
2024-06-15 12:40:06 -0700

    renamed lang file for new location, and changed setting location

--- settings.php
+++ settings.php
@@ -27,26 +27,35 @@ defined('MOODLE_INTERNAL') || die;
 
 
 if ($hassiteconfig) {
-    $ADMIN->add('server', new admin_category('tool_phoneverification_settings', new lang_string('pluginname', 'tool_phoneverification')));
-    $settingspage = new admin_settingpage('managetoolphoneverification', new lang_string('manage', 'tool_phoneverification'));
+    $ADMIN->add('server', new admin_category('phone', new lang_string('phone', 'tool_phoneverification')));
+    $settingspage = new admin_settingpage('managetoolphoneverification', new lang_string('phoneproviderconfiguration', 'tool_phoneverification'));
 
     if ($ADMIN->fulltree) {
+
         // $settingspage->add(new admin_setting_configcheckbox(
         //     'tool_phoneverification/showinnavigation',
         //     new lang_string('showinnavigation', 'tool_phoneverification'),
         //     new lang_string('showinnavigation_desc', 'tool_phoneverification'),
         //     1
         // ));
-        $settingspage->add(new admin_setting_configselect(
-            'tool_phoneverification/provider',
-            new lang_string('provider', 'tool_phoneverification'),
-            new lang_string('provider_desc', 'tool_phoneverification'),
+        // $settingspage->add(new admin_setting_configselect(
+        //     'tool_phoneverification/provider',
+        //     new lang_string('provider', 'tool_phoneverification'),
+        //     new lang_string('provider_desc', 'tool_phoneverification'),
+        //     'infobip',
+        //     array(
+        //         'infobip' => 'Infobip',
+        //         'twilio' => 'Twilio',
+        //         'awssns' => 'AWS SNS'
+        //     )
+        // ));
+
+        // Infobip
+        $link = html_writer::link('https://portal.infobip.com/', get_string('here', 'tool_phoneverification'));
+        $settingspage->add(new admin_setting_heading(
             'infobip',
-            array(
-                'infobip' => 'Infobip',
-                'twilio' => 'Twilio',
-                'awssns' => 'AWS SNS'
-            )
+            new lang_string('infobip', 'tool_phoneverification'),
+            new lang_string('infobip_desc', 'tool_phoneverification', $link)
         ));
         $settingspage->add(new admin_setting_configtext(
             'tool_phoneverification/infobipapikey',
@@ -62,21 +71,110 @@ if ($hassiteconfig) {
             new lang_string('infobipapibaseurl', 'tool_phoneverification'),
             new lang_string('infobipapibaseurl_desc', 'tool_phoneverification'),
             '',
-            PARAM_URL,
+            PARAM_URL
+        ));
+
+        // Twilio
+        $link = html_writer::link('https://www.twilio.com/', get_string('here', 'tool_phoneverification'));
+        $settingspage->add(new admin_setting_heading(
+            'twilio',
+            new lang_string('twilio', 'tool_phoneverification'),
+            new lang_string('twilio_desc', 'tool_phoneverification', $link)
+        ));
+        $settingspage->add(new admin_setting_configtext(
+            'tool_phoneverification/twilioaccountsid',
+            new lang_string('twilioaccountsid', 'tool_phoneverification'),
+            new lang_string('twilioaccountsid_desc', 'tool_phoneverification'),
+            '',
+            PARAM_TEXT,
             // '/^[a-f0-9]{32}-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/',
+            69
+        ));
+        $settingspage->add(new admin_setting_configtext(
+            'tool_phoneverification/twilioauthtoken',
+            new lang_string('twilioauthtoken', 'tool_phoneverification'),
+            new lang_string('twilioauthtoken_desc', 'tool_phoneverification'),
+            '',
+            PARAM_URL
+        ));
+        $settingspage->add(new admin_setting_configtext(
+            'tool_phoneverification/twilionumber',
+            new lang_string('twilionumber', 'tool_phoneverification'),
+            new lang_string('twilionumber_desc', 'tool_phoneverification'),
+            '',
+            PARAM_TEXT
+        ));
+
+        // AWSSNS
+        $link = html_writer::link('https://aws.amazon.com/sns/', get_string('here', 'tool_phoneverification'));
+        $settingspage->add(new admin_setting_heading(
+            'awssns',
+            new lang_string('awssns', 'tool_phoneverification'),
+            new lang_string('awssns_desc', 'tool_phoneverification', $link)
+        ));
+        $settingspage->add(new admin_setting_configtext(
+            'tool_phoneverification/awssnsaccesskey',
+            new lang_string('awssnsaccesskey', 'tool_phoneverification'),
+            new lang_string('awssnsaccesskey_desc', 'tool_phoneverification'),
+            '',
+            PARAM_TEXT,
+            69
+        ));
+        $settingspage->add(new admin_setting_configtext(
+            'tool_phoneverification/awssnssecretkey',
+            new lang_string('awssnssecretkey', 'tool_phoneverification'),
+            new lang_string('awssnssecretkey_desc', 'tool_phoneverification'),
+            '',
+            PARAM_TEXT,
+            69
+        ));
+        $settingspage->add(new admin_setting_configtext(
+            'tool_phoneverification/awssnsregion',
+            new lang_string('awssnsregion', 'tool_phoneverification'),
+            new lang_string('awssnsregion_desc', 'tool_phoneverification'),
+            '',
+            PARAM_TEXT,
             69
         ));
     }
 
-    // $settingspage->add(new admin_setting_config(
-    //     'tool_phoneverification/testinputs',
-    //     new lang_string('testinputs', 'tool_phoneverification'),
-    //     new lang_string('testinputs_desc', 'tool_phoneverification'),
-    //     new moodle_url('/tool/phoneverification/testinputs.php')
-    // ));
-
-    $ADMIN->add('toolplugins', $settingspage);
+    $ADMIN->add('phone', $settingspage);
 }
+
+// // Email.
+//     $ADMIN->add('phone', new admin_category('email', new lang_string('categoryemail', 'admin')));
+
+//     // Outgoing mail configuration.
+//     $temp = new admin_settingpage('outgoingmailconfig', new lang_string('outgoingmailconfig', 'admin'));
+
+//     if (!empty($CFG->noemailever)) {
+//         $noemaileverwarning = new \core\output\notification(get_string('noemaileverwarning', 'admin'),
+//         \core\output\notification::NOTIFY_ERROR);
+//         $temp->add(new admin_setting_heading('outgoingmaildisabled', '', $OUTPUT->render($noemaileverwarning)));
+//     }
+
+//     $temp->add(new admin_setting_heading('smtpheading', new lang_string('smtp', 'admin'),
+//         new lang_string('smtpdetail', 'admin')));
+
+//     $temp->add(new admin_setting_configtext('smtphosts', new lang_string('smtphosts', 'admin'),
+//         new lang_string('configsmtphosts', 'admin'), '', PARAM_RAW));
+
+//     $options = [
+//         '' => new lang_string('none', 'admin'),
+//         'ssl' => 'SSL',
+//         'tls' => 'TLS',
+//     ];
+
+//     $temp->add(new admin_setting_configselect('smtpsecure', new lang_string('smtpsecure', 'admin'),
+//         new lang_string('configsmtpsecure', 'admin'), '', $options));
+
+//     $authtypeoptions = [
+//         'LOGIN' => 'LOGIN',
+//         'PLAIN' => 'PLAIN',
+//         'NTLM' => 'NTLM',
+//         'CRAM-MD5' => 'CRAM-MD5',
+//     ];
+
 
 // if ($ADMIN->fulltree) {
 //     // INFOBIP_API_KEY setting.
